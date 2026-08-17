4fc64e674e30c4ea8549fc05eab04c16d21154f9
Farhan-Khan7 <fk5518744@gmail.com>
2026-08-18 02:11:43 +0530

    I have create my first server

--- Day 01/server.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-
-let server = http.createServer((req , res) => {
-  res.end("okay me server ke paas se aa rha hu !")
-})
-
-server.listen(3000, () => {
-    console.log("Server is start ")
-})
