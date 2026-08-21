bfce72849ad32b4856de595c555f653002d54866
Farhan-Khan7 <fk5518744@gmail.com>
2026-08-21 22:19:26 +0530

    I have done

--- Day 03/script.js
+++ Day 03/script.js
@@ -1,11 +1,30 @@
 const express = require('express');
 
-
 const app = express();
 
+app.use(express.json())
 
 
+
+app.get('/' , (req , res) => {
+    res.send("FInall sever chalu ho gya ")
+})
+
+
+let notes = [];
+
+app.post('/notes' , (req , res) => {
+    
+    notes.push(req.body)
+    res.send("Note Created")
+})
+
+app.get('/getNotes' , (req , res) => {
+    res.send(notes)
+})
 
 
 const port = 8080;
-app.listen(port)
+app.listen(port , () => {
+    console.log(`Server is running on port : ${port}`)
+})
